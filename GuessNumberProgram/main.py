9a152888b650873488b1dff4c51e10a9b000c5a1
Henry Cho <34312635+hankcho@users.noreply.github.com>
2023-04-17 21:43:32 +0000

    added exception handling

--- GuessNumberProgram/main.py
+++ GuessNumberProgram/main.py
@@ -14,17 +14,20 @@ def pick():
     correct_number = random.randint(1, 100)
     guess_count = 1
 
-    while guess != correct_number:
-        if guess < correct_number:
-            guess = int(
-                input("Wrong. You need to guess higher. What is your guess?: "))
-        else:
-            guess = int(
-                input("Wrong. You need to guess lower. What is your guess?: "))
-        guess_count += 1
+    try:
+        while guess != correct_number:
+            if guess < correct_number:
+                guess = int(
+                    input("Wrong. You need to guess higher. What is your guess?: "))
+            else:
+                guess = int(
+                    input("Wrong. You need to guess lower. What is your guess?: "))
+            guess_count += 1
 
-    print(f"\n CONGRATS {self.upper()}! The correct number is {correct_number}. \
-          \n It took you {guess_count} guesses.")
+        print(f"\n CONGRATS {self.upper()}! The correct number is {correct_number}. \
+            \n It took you {guess_count} guesses.")
+    except:
+        print("You did not make a guess.  Please restart the game.")
 
 if __name__ == "__main__":
     name()
